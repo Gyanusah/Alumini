10db153e765992ec0e7e7a8f718d023143a343ba
Gyanendra Shah <shahgyanendra555@gmail.com>
2025-09-22 09:38:29 +0530

    first commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import HeroSection from "../page/heroSection";
+import HeroSection from "../Page/heroSection";
 
-import AlumniDirectory from "../page/AlumniDirectory";
+import AlumniDirectory from "../Page/AlumniDirectory";
 
-import UpComingEvents from "../page/UpComingEvents";
+import UpComingEvents from "../Page/UpComingEvents";
 
-import EventsCard from "../page/EventsCard";
+import EventsCard from "../Page/EventsCard";
 import NewsUpdates from "./News/NewsUpdates";
 import AlumniNews from "./News/News";
-import WaysToGive from "../page/WayToGive";
-import SupportSection from "../page/Donatioin/Donation";
+import WaysToGive from "../Page/WayToGive";
+import SupportSection from "../Page/Donatioin/Donation";
 
 export default function Home() {
   return (
